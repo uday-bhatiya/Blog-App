cb556a9175a8fb3775968f822f9df76d6cb19327
Uday Bhatiya <udaybhatiya31@gmail.com>
2024-06-08 13:04:38 +0530

    sign up functionality

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,7 +1,41 @@
-import React from 'react'
-import {Link} from "react-router-dom"
+import React, { useState } from 'react'
+import {Link, useNavigate} from "react-router-dom"
 
 const Signup = () => {
+
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate()
+
+  const handleChange = (e) => {
+    // console.log(e.target)
+    setFormData(
+      {...formData, [e.target.id]: e.target.value.trim() }
+    )
+  }
+  // console.log(formData)
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.email || !formData.password) {
+      return setErrorMessage("Please fill out all fields")
+    }
+    try {
+      const res = await fetch('/api/v1/user/signup', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json'},
+        body: JSON.stringify(formData)
+      })
+      const data =   await res.json()
+      if (res.ok) {
+        navigate('/sign-in')
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <div className='min-h-screen pt-20 bg-white dark:bg-gray-800 '>
       <div className='flex flex-col p-3 max-w-3xl mx-auto md:flex-row md:items-start md:justify-center md:gap-5'>
@@ -13,18 +47,18 @@ const Signup = () => {
         </div>
 
         <div className='flex-1'>
-          <form className='flex flex-col gap-4'> 
+          <form className='flex flex-col gap-4' onSubmit={handleSubmit}> 
             <div>
             <label className='block dark:text-white'>Username</label>
-            <input className='w-full p-2 border-solid border-2 border-slate-200 rounded-md' type="text" placeholder='username' />
+            <input id='username' className='w-full p-2 border-solid border-2 border-slate-200 rounded-md' type="text" placeholder='username' onChange={handleChange} />
             </div>
             <div>
             <label className='block dark:text-white'>Email</label>
-            <input className='w-full p-2 border-solid border-2 border-slate-200 rounded-md' type="email" placeholder='email' />
+            <input id='email' className='w-full p-2 border-solid border-2 border-slate-200 rounded-md' type="email" placeholder='email'onChange={handleChange} />
             </div>
             <div>
             <label className='block dark:text-white'>Password</label>
-            <input className='w-full p-2 border-solid border-2 border-slate-200 rounded-md' type="password" placeholder='password' />
+            <input id='password' className='w-full p-2 border-solid border-2 border-slate-200 rounded-md' type="password" placeholder='password'onChange={handleChange} />
             </div>
 
             <button
@@ -36,6 +70,13 @@ const Signup = () => {
           <div className=' mt-5 text-sm dark:text-white'>
             <span>Already have an account? <Link className='text-blue-600' to="/sign-in">Sign in</Link> </span>
           </div>
+          {
+            errorMessage && (
+              <div className='w-full bg-red-400 py-2 px-1 text-black rounded-md mt-2'>
+                {errorMessage}
+              </div>
+            )
+          }        
         </div>
       </div>
     </div>
